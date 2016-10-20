6b1a63edc58b0bca9a495556f34e4a666d22e42e
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-20 08:18:15 -0400

    Initial commit for new repo

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+﻿var fortuneArray = ["Not Likely", "You Bet", "Doubtful", "Eh...Why not?", "Please...", "Yep"];
+
+function getFortune() {
+    document.getElementById("result").innerHTML = fortuneArray [Math.floor(Math.random()*6)]; 
+}
+
+
+
